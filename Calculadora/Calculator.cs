3655581d1f24d7ff946545071ff4362de7b09ff5
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-07-19 17:07:19 -0300

    Add Calculator class

---
+++ Calculadora/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+        
+        public double Add(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if(divisor == 0)
+                throw new DivideByZeroException();
+            return dividend / divisor;
+        }
+    }
+}
