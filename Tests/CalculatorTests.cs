5c45cb903c35a8dd0d69b80d2f6ee2f60413de8c
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-07-19 17:08:52 -0300

    Add CalculatorTests class

---
+++ Tests/CalculatorTests.cs
@@ -0,0 +1,52 @@
+namespace Tests
+{
+    using NUnit.Framework;
+    using Calculadora;
+
+    [TestFixture]
+    [Author("Victor Ercilio")]
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EmptyConstructor_ReturnsCalculatorObject()
+        {
+            Calculator calc;
+
+            calc = new Calculator();
+
+            Assert.That(calc, Is.Not.Null);
+            Assert.That(calc, Is.InstanceOf<Calculator>());
+        }
+
+        [Test]
+        public void Add_AddTwoNumbers_ReturnsSumOfThem()
+        {
+            Calculator calc = new Calculator();
+            double firstNumber = 1;
+            double secondNumber = 2;
+            double sumResult;
+
+            sumResult = calc.Add(firstNumber, secondNumber);
+            
+            Assert.That(sumResult, Is.EqualTo(firstNumber + secondNumber));
+        }
+
+        [Test]
+        public void Divide_NumberDividedByZero_ThrowsDivideByZeroException()
+        {
+            Calculator calc = new Calculator();
+
+            TestDelegate division = () => calc.Divide(3, 0);
+
+            _ = Assert.Throws<DivideByZeroException>(division);
+        }
+
+        [Test]
+
+    }
+}
