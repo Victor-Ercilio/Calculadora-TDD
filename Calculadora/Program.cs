19c7ab21254fe510b0bb28083777f6e697da3ce7
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2024-07-20 11:41:20 -0300

    Update main program

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculator calc = new();
+            ConsoleKeyInfo response;
+
+            do
+            {
+                Console.Write("Primeiro numero: ");
+                double.TryParse(Console.ReadLine(), out double firstNumber);
+
+                Console.Write("Segundo numero: ");
+                double.TryParse(Console.ReadLine(), out double secondNumber);
+
+                Console.Write("Operação: ");
+                response = Console.ReadKey();
+                Console.WriteLine();
+                try
+                {
+                    switch (response.Key)
+                    {
+                        case ConsoleKey.Add:
+                            Console.WriteLine(
+                                $"{firstNumber} + {secondNumber} = {calc.Add(firstNumber, secondNumber)}");
+                            break;
+                        case ConsoleKey.Subtract:
+                            Console.WriteLine(
+                                $"{firstNumber} - {secondNumber} = {calc.Sub(firstNumber, secondNumber)}");
+                            break;
+                        case ConsoleKey.Multiply:
+                            Console.WriteLine(
+                                $"{firstNumber} * {secondNumber} = {calc.Multiply(firstNumber, secondNumber)}");
+                            break;
+                        case ConsoleKey.Divide:
+                            Console.WriteLine(
+                                $"{firstNumber} / {secondNumber} = {calc.Divide(firstNumber, secondNumber)}");
+                            break;
+                        default:
+                            Console.WriteLine("Operação não disponível");
+                            break;
+                    }
+                }
+                catch(DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                }
+                catch(ArithmeticException ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                }
+                
+                Console.Write("Repetir (S/N)? ");
+                response = Console.ReadKey();
+                Console.WriteLine();
+            } while (response.Key == ConsoleKey.S);
         }
     }
 }
